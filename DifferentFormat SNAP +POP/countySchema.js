5c7d5f7b6ef59590485c7735b5249e8ae8503ef1
Sabelo954 <sabelomthembu472@gmail.com>
2021-01-07 21:22:56 -0500

    Update countySchema.js
    Formatting updated

--- DifferentFormat SNAP +POP/countySchema.js
+++ DifferentFormat SNAP +POP/countySchema.js
@@ -1,88 +1,118 @@
-export var thisis__ = [
+https://api.census.gov/data/2019/acs/acs5/profile?get=NAME,DP05_0001E&for=county:005&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853export
+var thisis__ = [
   {
     AnneArundelCounty: {
-      snap: [
+      MedianHincome: [
         {
           year: 2015,
           url:
-            'https://api.census.gov/data/2015/acs/acs5?get=NAME,B19058_002E&for=county:003&in=state:24',
+            'https://api.census.gov/data/2015/acs/acs5/subject?get=NAME,S1901_C01_012E&for=county:003&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853',
         },
         {
           year: 2016,
           url:
-            'https://api.census.gov/data/2016/acs/acs5?get=NAME,B19058_002E&for=county:003&in=state:24',
+            'https://api.census.gov/data/2016/acs/acs5/subject?get=NAME,S1901_C01_012E&for=county:003&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853',
         },
         {
           year: 2017,
           url:
-            'https://api.census.gov/data/2015/acs/acs5?get=NAME,B19058_002E&for=county:003&in=state:24',
+            'https://api.census.gov/data/2017/acs/acs5/subject?get=NAME,S1901_C01_012E&for=county:003&in=state:24&key=855666deab62d95596011f1944d9f1bd8c9188534',
         },
         {
           year: 2018,
           url:
-            'https://api.census.gov/data/2018/acs/acs5?get=NAME,B19058_002E&for=county:003&in=state:24',
+            'https://api.census.gov/data/2018/acs/acs5/subject?get=NAME,S1901_C01_012E&for=county:003&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853',
         },
         {
           year: 2019,
           url:
-            'https://api.census.gov/data/2019/acs/acs5?get=NAME,B19058_002E&for=county:003&in=state:24',
+            'https://api.census.gov/data/2019/acs/acs5/subject?get=NAME,S1901_C01_012E&for=county:003&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853',
         },
       ],
+
+      pop: [
+        {
+          year: 2015,
+          url:
+            'https://api.census.gov/data/2015/acs/acs5/profile?get=NAME,DP05_0001E&for=county:005&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853',
+        },
+        {
+          year: 2016,
+          url:
+            'https://api.census.gov/data/2016/acs/acs5/profile?get=NAME,DP05_0001E&for=county:005&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853',
+        },
+        {
+          year: 2017,
+          url:
+            'https://api.census.gov/data/2017/acs/acs5/profile?get=NAME,DP05_0001E&for=county:005&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853',
+        },
+        {
+          year: 2018,
+          url:
+            'https://api.census.gov/data/2018/acs/acs5/profile?get=NAME,DP05_0001E&for=county:005&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853',
+        },
+        {
+          year: 2019,
+          url:
+            'https://api.census.gov/data/2019/acs/acs5/profile?get=NAME,DP05_0001E&for=county:005&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853',
+        },
+      ],
+
     },
 
     BaltimoreCounty: {
-      snap: [
+      MedianHincome: [
         {
           year: 2015,
           url:
-            'https://api.census.gov/data/2015/acs/acs5?get=NAME,B19058_002E&for=county:005&in=state:24',
+            'https://api.census.gov/data/2015/acs/acs5/subject?get=NAME,S1901_C01_012E&for=county:005&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853',
         },
         {
           year: 2016,
           url:
-            'https://api.census.gov/data/2016/acs/acs5?get=NAME,B19058_002E&for=county:005&in=state:24',
+            'https://api.census.gov/data/2015/acs/acs5/subject?get=NAME,S1901_C01_012E&for=county:005&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853',
         },
         {
           year: 2017,
           url:
-            'https://api.census.gov/data/2017/acs/acs5?get=NAME,B19058_002E&for=county:005&in=state:24',
+            'https://api.census.gov/data/2015/acs/acs5/subject?get=NAME,S1901_C01_012E&for=county:005&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853',
         },
         {
           year: 2018,
           url:
-            'https://api.census.gov/data/2018/acs/acs5?get=NAME,B19058_002E&for=county:005&in=state:24',
+            'https://api.census.gov/data/2015/acs/acs5/subject?get=NAME,S1901_C01_012E&for=county:005&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853',
         },
         {
           year: 2019,
           url:
-            'https://api.census.gov/data/2019/acs/acs5?get=NAME,B19058_002E&for=county:005&in=state:24',
+            'https://api.census.gov/data/2015/acs/acs5/subject?get=NAME,S1901_C01_012E&for=county:005&in=state:24&key=855666deab62d95596011f1944d9f1bd8c9188534',
         },
       ],
       pop: [
         {
           year: 2015,
           url:
-            'https://api.census.gov/data/2015/acs/acs5/profile?get=NAME,DP05_0001E&for=county:005&in=state:24',
+            'https://api.census.gov/data/2015/acs/acs5/profile?get=NAME,DP05_0001E&for=county:005&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853',
         },
         {
           year: 2016,
           url:
-            'https://api.census.gov/data/2016/acs/acs5/profile?get=NAME,DP05_0001E&for=county:005&in=state:24',
+            'https://api.census.gov/data/2016/acs/acs5/profile?get=NAME,DP05_0001E&for=county:005&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853',
         },
         {
           year: 2017,
           url:
-            'https://api.census.gov/data/2016/acs/acs5/profile?get=NAME,DP05_0001E&for=county:005&in=state:24',
+            'https://api.census.gov/data/2017/acs/acs5/profile?get=NAME,DP05_0001E&for=county:005&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853',
         },
         {
           year: 2018,
           url:
-            'https://api.census.gov/data/2016/acs/acs5/profile?get=NAME,DP05_0001E&for=county:005&in=state:24',
+            'https://api.census.gov/data/2018/acs/acs5/profile?get=NAME,DP05_0001E&for=county:005&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853',
         },
         {
           year: 2019,
           url:
-            'https://api.census.gov/data/2019/acs/acs5/profile?get=NAME,DP05_0001E&for=county:005&in=state:24',
+            'https://api.census.gov/data/2019/acs/acs5/profile?get=NAME,DP05_0001E&for=county:005&in=state:24&key=855666deab62d95596011f1944d9f1bd8c918853',
         },
       ],
     },
