b43da1b8ac282d37d82e09c9f44530af4e581a1b
Sabelo954 <sabelomthembu472@gmail.com>
2021-01-10 17:41:46 -0500

    Updated county information
    Updated county info

--- DifferentFormat SNAP +POP/countyfinalversion.js
+++ DifferentFormat SNAP +POP/countyfinalversion.js
@@ -1,4 +1,4 @@
-MedianHincomeimport countySchema from 'countySchema.js';
+import countySchema from 'countySchema.js';
 
 
 // API endpoints for each graph
@@ -114,54 +114,68 @@ populationChart.addSeries(null, dimple.plot.bar);
 populationChart.draw();
 }
 
+//These graphs will use data from an excel table not from API
 
-
-
-  // Get SNap
-  getSnap();
-}
-
-getSnap(){
-  snapObect = countySchema[currentCounty].snap;
-  var returnObject = {}
-  // Structure of SNap object
-  /*
-    snap = {
-    1: {
-      year: 2019
-      url: "thisistheurl.com"
-    }
-  }
-  */
-  // Loop through the snap object and get data for every year
-   for (var i = 0; i < snapObect.length; i++) {
-     var snap = snapObect[i];
-     var urlpop= snap.url;
-     var year = snap.year;
-     d3.json(urlpop).get(function(error,data){
-       var dataSNAPbeg= data[1];
-       var dataSNAP= dataSNAPbeg.map(Number).slice(1,2);
-       console.log(dataSNAP);
-       // Set the data response for this year into the return object
-       returnObject[year] = dataSNAP;
-     });
-   }
-   // TODO: Might need to put the above function in a promise
-   // Instead of returning, we can just call the update snap char fnx immediately
-   updateSnapChart()
-}
-
-updateSnapChart(data) {
-  var svg1 = dimple.newSvg("#svgPopulation", chartSizewidth, chartSizeheight);
-  var data = [
-    { "Year":"2017", "SNAP Recipients": data["2017"] },
-    { "Year":"2018", "SNAP Recipients": data["2018"] },
-    { "Year":"2019", "SNAP Recipients": data["2019"]}
-  ];
-  var populationChart = new dimple.chart(svg1, data);
-  populationChart.addCategoryAxis("x", "Year");
-  var y=populationChart.addMeasureAxis("y", "SNAP Recipients");
-  y.tickFormat = ',.0f';
-  populationChart.addSeries(null, dimple.plot.bar);
-  populationChart.draw();
-}
+// updateSnapChart(data) {
+//   var svg1 = dimple.newSvg("#svgPopulation", chartSizewidth, chartSizeheight);
+//   var data = [
+//     { "Year":"2017", "SNAP Recipients": data["2017"] },
+//     { "Year":"2018", "SNAP Recipients": data["2018"] },
+//     { "Year":"2019", "SNAP Recipients": data["2019"]}
+//   ];
+//   var populationChart = new dimple.chart(svg1, data);
+//   populationChart.addCategoryAxis("x", "Year");
+//   var y=populationChart.addMeasureAxis("y", "SNAP Recipients");
+//   y.tickFormat = ',.0f';
+//   populationChart.addSeries(null, dimple.plot.bar);
+//   populationChart.draw();
+// }
+//
+// // Get SNap
+// getSnap();
+// }
+//
+// getSnap(){
+// snapObect = countySchema[currentCounty].snap;
+// var returnObject = {}
+// // Structure of SNap object
+// /*
+//   snap = {
+//   1: {
+//     year: 2019
+//     url: "thisistheurl.com"
+//   }
+// }
+// */
+// // Loop through the snap object and get data for every year
+//  for (var i = 0; i < snapObect.length; i++) {
+//    var snap = snapObect[i];
+//    var urlpop= snap.url;
+//    var year = snap.year;
+//    d3.json(urlpop).get(function(error,data){
+//      var dataSNAPbeg= data[1];
+//      var dataSNAP= dataSNAPbeg.map(Number).slice(1,2);
+//      console.log(dataSNAP);
+//      // Set the data response for this year into the return object
+//      returnObject[year] = dataSNAP;
+//    });
+//  }
+//  // TODO: Might need to put the above function in a promise
+//  // Instead of returning, we can just call the update snap char fnx immediately
+//  updateSnapChart()
+// }
+//
+// updateSnapChart(data) {
+// var svg1 = dimple.newSvg("#svgPopulation", chartSizewidth, chartSizeheight);
+// var data = [
+//   { "Year":"2017", "SNAP Recipients": data["2017"] },
+//   { "Year":"2018", "SNAP Recipients": data["2018"] },
+//   { "Year":"2019", "SNAP Recipients": data["2019"]}
+// ];
+// var populationChart = new dimple.chart(svg1, data);
+// populationChart.addCategoryAxis("x", "Year");
+// var y=populationChart.addMeasureAxis("y", "SNAP Recipients");
+// y.tickFormat = ',.0f';
+// populationChart.addSeries(null, dimple.plot.bar);
+// populationChart.draw();
+// }
