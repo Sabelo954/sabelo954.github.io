faaf7458355b781a704c68c79a11acb3f67a35de
Sabelo954 <sabelomthembu472@gmail.com>
2021-03-10 18:26:35 -0500

    updating

--- DLLR-Dashboard - with 2019 dropwdowns working/DLLR-Dashboard updated/counties_external_code.c47ae50003996d4c3fe7.js
+++ DLLR-Dashboard - with 2019 dropwdowns working/DLLR-Dashboard updated/counties_external_code.c47ae50003996d4c3fe7.js
@@ -1968,7 +1968,7 @@
                       },
                       {
                         chart: tanf_attainment_chart,
-                        bounds: [45, "12%", "15%", "65%"],
+                        bounds: [45, "12%", "15%", "5%"],
                         categoryAxis: ["x", "Time"],
                         xtitle: " ",
                         order: C,
